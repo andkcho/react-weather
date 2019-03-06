03371f140cc0f08a190c1a751e27e57322fdbd41
andkcho <andrewc801@gmail.com>
2019-03-05 20:43:08 -0700

    api key update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Weather from "./components/Weather";
 const API_KEY = "d3954a5ce5ca11a2f4e08168d66ff520";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}
-      &units=metric`);
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
         
     const data = await api_call.json();
     console.log(data);
@@ -18,11 +18,11 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
   }
 };
 
-export default App;
+export default App; 
